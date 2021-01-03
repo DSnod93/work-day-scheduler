2e73f30ad97a7d633341be9fbc9c95922c395bc5
DSnoddy <douglassnoddyjr@gmail.com>
2021-01-03 12:40:07 -0800

    Added function to stay up to date with current hour

--- Develop/script.js
+++ Develop/script.js
@@ -5,17 +5,39 @@ $("#currentDay").text(moment().format("dddd, MMMM Do"));
  
 
 
+function timeTracker() {
 
-});
+    // current hour using moment.js
+    var currentHour = moment().hour();
+
+    // loop over time blocks
+    $(".time-block").each(function() {
+    var currentHour = parseInt($(this).attr("id").split("hour")[1]);
+
+    //color code each time block to indicate if it is past, present, or future
+    if (currentHour < currentHour) {
+        $(this).addClass("past");
+        $(this).removeClass("future");
+        $(this).removeClass("present");
+    }
+    else if (currentHour === currentHour) {
+        $(this).removeClass("past");
+        $(this).addClass("future");
+        $(this).removeClass("present");
+    }
+    else {
+        $(this).removeClass("past");
+        $(this).removeClass("future");
+        $(this).addClass("present");
+    }
+})
+
+}
+// repeats itself
+timeTracker();
+})
 
 
-
-
-
-//create time blocks for business hours
-
-
-//color code each time block to indicate if it is past, present, or future
 // user can enter an event when clicking time block
 //when user clicks save, the event is saved to local storage
 // refereshing the page still shows saved events
