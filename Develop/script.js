b80aafc56f0112f4683227a9fa7a2e9774e4c799
DSnoddy <douglassnoddyjr@gmail.com>
2021-01-10 10:21:19 -0800

    Added items to local storage

--- Develop/script.js
+++ Develop/script.js
@@ -7,11 +7,28 @@ $("#currentDay").text(moment().format("dddd, MMMM Do"));
 $(".saveBtn").on("click", function() {
     // get values
     console.log(this);
+    // get the change from the sibling html description attribute
+    var text = $(this).siblings(".description").val();
+    // get the change from the html id attribute
+    var time = $(this).parent().attr("id")
 
-
-    localStorage.setItem(); 
+    //when user clicks save, the event is saved to local storage
+    localStorage.setItem(time, text); 
     
 })
+
+// load saved data from local storage
+$("#hour8 .description").val(localStorage.getItem("hour8"));
+$("#hour9 .description").val(localStorage.getItem("hour9"));
+$("#hour10 .description").val(localStorage.getItem("hour10"));
+$("#hour11 .description").val(localStorage.getItem("hour11"));
+$("#hour12 .description").val(localStorage.getItem("hour12"));
+$("#hour13 .description").val(localStorage.getItem("hour13"));
+$("#hour14 .description").val(localStorage.getItem("hour14"));
+$("#hour15 .description").val(localStorage.getItem("hour15"));
+$("#hour16 .description").val(localStorage.getItem("hour16"));
+$("#hour17 .description").val(localStorage.getItem("hour17"));
+
 
 function timeTracker() {
 
@@ -39,12 +56,7 @@ function timeTracker() {
         $(this).addClass("present");
     }
 })
-
+}
 // repeats itself
 timeTracker();
-};
-
-
-// user can enter an event when clicking time block
-//when user clicks save, the event is saved to local storage
-// refereshing the page still shows saved events
+})
